1fc80fa161fff8a542b52946ef5b97311a583f4d
Lucas Bowens <kingkoop4evr@gmail.com>
2018-03-05 19:11:05 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -1,19 +1,48 @@
 function basicTeenager(age) {
+  var age = x
+if (x >= 13 && x<= 19){
+  return "You are a teenager!"
 
+}else{
+  return undefined
+}
 }
 
 function teenager(age) {
-
+  if(age >= 13 && age <= 19){
+    return 'You are a teenager!'
+  }else{
+    return 'You are not a teenager!'
+  }
 }
 
 function ageChecker(age) {
-
+If(age >= 13 && age <= 19){
+  return 'You are a Teenager!'
+}else if(age <= 12){
+  return 'You are a kid'
+}else if(age >= 20){
+  return 'You are a grownup'
+}else{
+  return 'What is your age again?'
+}
 }
 
 function ternaryTeenager(age) {
-
+return (age >= 13 && age <= 19) ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
+switch (age >=13 && age <= 19){
+  case x:
+  return "You are a teenager"
+  break;
+  case y:
+    return "You are not a teenager"
+    break;
+    default:
+    false
+
+}
 
 }
